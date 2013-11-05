7ed1866afec70630ae31737534d468d0c361f823
joetamburro <joetamburro38@gmail.com>
2013-11-05 15:40:46 -0500

    hooked up slide.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
 console.log('\'Allo \'Allo!');
+
+    $(function(){
+      $("#slides").slidesjs({
+        width: 940,
+        height: 680,
+        play: {
+          active: true,
+          auto: true,
+          interval: 4000,
+          swap: true
+        },
+        navigation: {
+          active: true,
+          effect: "slide"
+        }
+      });
+    });
