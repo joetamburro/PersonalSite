d1a2100609073d906b25b2669c32988f8e445486
joetamburro <joetamburro38@gmail.com>
2013-11-06 12:28:10 -0500

    hooked up smooth scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,30 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+  $("#slides").slidesjs({
+    width: 940,
+    height: 680,
+    play: {
+      active: true,
+      auto: true,
+      interval: 4000,
+      swap: true
+    },
+    navigation: {
+      active: true,
+      effect: "slide"
+    }
+  });
 
-    $(function(){
-      $("#slides").slidesjs({
-        width: 940,
-        height: 680,
-        play: {
-          active: true,
-          auto: true,
-          interval: 4000,
-          swap: true
-        },
-        navigation: {
-          active: true,
-          effect: "slide"
-        }
-      });
-    });
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+})
