b2f436d5fc7c68f56e7db99bbeb55051a0ffd41f
joetamburro <joetamburro38@gmail.com>
2013-11-06 22:09:50 -0500

    swapped out sliders

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,30 @@
-$(function(){
-  $("#slides").slidesjs({
-    width: 940,
-    height: 680,
-    play: {
-      active: true,
-      auto: true,
-      interval: 4000,
-      swap: true
-    },
-    navigation: {
-      active: true,
-      effect: "slide"
-    }
+// $(function(){
+//   $("#slides").slidesjs({
+//     width: 900,
+//     height: 680,
+//     play: {
+//       active: true,
+//       auto: true,
+//       interval: 3000,
+//       swap: true
+//     },
+//     fade: {
+//         speed: 300,
+//           // [number] Speed in milliseconds of the fade animation.
+//         crossfade: true
+//           // [boolean] Cross-fade the transition.
+//       },
+//     // navigation: {
+//     //   active: true,
+//     //   effect: "fade"
+//     // }
+//   });
+$(document).ready(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    controlNav: "thumbnails"
   });
+});
 
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -26,5 +38,3 @@ $(function(){
       }
     }
   });
-
-})
